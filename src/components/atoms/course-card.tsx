1e78e92cb8250df3c4596317d06445fedd096928
Andrew Radionov <andrey.radioniv.2003@gmail.com>
2023-04-15 20:44:22 +0700

    :sparkles: feat(ui): Add my cources page and teaching page
    - Create reusable course card component for both pages
    - In course card create buttons of statuses with different colors
    - Create conversions to russian lang for status text

---
+++ src/components/atoms/course-card.tsx
@@ -0,0 +1,48 @@
+import { Course } from "@/services/courcesApi";
+import { FC } from "react";
+
+const CourseCard: FC<Course> = (course) => {
+  const {
+    name,
+    startYear,
+    semester,
+    maximumStudentsCount,
+    remainingSlotsCount,
+    status,
+  } = course;
+
+  const statusColors = {
+    Created: "text-emerald-500",
+    OpenForAssigning: "text-blue-400",
+    Started: "text-gray-600	",
+    Finished: "text-red-500",
+  };
+
+  const statusConvertions = {
+    Created: "Создан",
+    OpenForAssigning: "Открыт для записи",
+    Started: "В процессе обучения",
+    Finished: "Закрыт",
+  };
+
+  return (
+    <div className="flex justify-between font-montserrat  border-solid border border-slate-300 p-4">
+      <div className="w-[80%]">
+        <h2 className="mt-0 mb-0 font-medium text-xl w-[80%]">{name}</h2>
+        <p className="text-md mt-2">Учебный год - {startYear}</p>
+        <p className="text-md mt-1">Семестр - {semester}</p>
+        <p className="text-xs mt-2 text-current">
+          Мест всего - {maximumStudentsCount}
+        </p>
+        <p className="text-xs mt-1 text-current">
+          Мест свободно - {remainingSlotsCount}
+        </p>
+      </div>
+      <p className={`${statusColors[status]} font-medium`}>
+        {statusConvertions[status]}
+      </p>
+    </div>
+  );
+};
+
+export default CourseCard;
