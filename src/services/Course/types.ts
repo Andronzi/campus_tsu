d9c442b16f8e2b8bcb14a206c2107932f010993e
Andronzi <andrey.radioniv.2003@gmail.com>
2023-04-22 13:18:56 +0700

    :sparkles: feat(custom): Add some endpoints to course api and add some types
    - Add getting course with details, adding course, notifications and teacher
    - Add Student, Teacher, Notification, AddTeacher, AddNotification, CourseDetails types

---
+++ src/services/Course/types.ts
@@ -0,0 +1,61 @@
+type Status = "Created" | "OpenForAssigning" | "Started" | "Finished";
+
+type Semester = "Spring" | "Autumn"
+
+export type Course = {
+    id: string;
+    name: string;
+    startYear: number;
+    maximumStudentsCount: number;
+    remainingSlotsCount: number;
+    status: Status;
+    semester: Semester;
+  }
+  
+  
+type ExcludeFromCourse = "id" | "remainingSlotsCount" | "status";
+export type CourseRequest = Omit<Course, ExcludeFromCourse> & { 
+    groupId: string, 
+    requirements: string, 
+    annotations: string, 
+    mainTeacherId: string 
+}
+
+export type Student = {
+    id: string;
+    name: string;
+    email: string;
+    status: string;
+    midtermResult: string;
+    finalResult: string;
+}
+
+export type Teacher = {
+    name: string;
+    email: string;
+    isMain: boolean;
+}
+
+export type Notification = {
+    text: string;
+    isImportant: boolean;
+}
+
+type CourseDetailsWithExtraFields = Course & ExcludeFromCourse & {
+    studentsEnrolledCount: number;
+    studentsInQueueCount: number;
+    students: Student[],
+    teachers: Teacher[],
+    notifications: Notification[],
+}
+
+export type CourseDetails = Omit<CourseDetailsWithExtraFields, "mainTeacherId">
+
+export type AddTeacherRequest = {
+    userId: string;
+    courseId: string;
+}
+
+export type AddNotification = {
+    courseId: string;
+} & Notification;
