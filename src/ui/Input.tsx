284e6ad56cbebc26056035aa0d42704b975d4286
Andronzi <andrey.radioniv.2003@gmail.com>
2023-04-22 13:06:32 +0700

    :recycle: refactor(ui): Make some minor changes to Input
    - Make label optional and wrap input in label
    - Add value and className params
    - Add radio as input type
    - Add styles for radio input

--- src/ui/Input.tsx
+++ src/ui/Input.tsx
@@ -5,34 +5,49 @@ import {
   UseFormRegister,
 } from "react-hook-form";
 
-type InputType = "email" | "text" | "password" | "date";
+type InputType = "email" | "text" | "password" | "date" | "radio";
 
 type InputProps<T extends FieldValues> = {
-  label: string;
+  label?: string;
   name: Path<T>;
+  value?: string | boolean;
   type: InputType;
   errors: any;
   register?: UseFormRegister<T>;
   rules?: RegisterOptions;
+  className: string;
 };
 
 const Input = <T extends FieldValues>({
   label,
   name,
+  value,
   type,
   errors,
   register,
   rules,
+  className,
 }: InputProps<T>) => (
-  <div className="mt-4 w-60">
-    <label className="block font-montserrat">{label}</label>
-    <input
-      type={type}
-      className="mt-2 border-1 border-slate-200 rounded-md border-solid 
+  <div className="mt-4">
+    <label
+      className={`block font-montserrat ${
+        type === "radio" && "flex items-center"
+      }`}
+      htmlFor={name}
+    >
+      <p>{label}</p>
+      <input
+        id={name}
+        value={value}
+        type={type}
+        className={`${
+          type !== "radio" && "mt-2"
+        } border-1 border-slate-200 rounded-md border-solid 
       text-md py-2 pl-1 text-slate-800 outline-0 focus:ring-1 
-      ring-blue-300 font-montserrat w-full"
-      {...(register && register(name, rules))}
-    />
+      ring-blue-300 font-montserrat w-60 ${className}`}
+        {...(register && register(name, rules))}
+      />
+    </label>
     {errors[name] && (
       <p className="mt-1 mb-0 font-montserrat text-red-400">
         {errors[name].message}
