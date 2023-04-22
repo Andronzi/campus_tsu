973e7608c91af143a9868adae40428e180b2f901
Andronzi <andrey.radioniv.2003@gmail.com>
2023-04-22 12:57:40 +0700

    :recycle: refactor(custom): Put cources conversions to data of project

---
+++ src/pages/data/course.ts
@@ -0,0 +1,18 @@
+export const statusColors = {
+    Created: "text-gray-600",
+    OpenForAssigning: "text-emerald-500",
+    Started: "text-blue-400",
+    Finished: "text-red-500",
+  };
+
+export const statusConvertions = {
+    Created: "Создан",
+    OpenForAssigning: "Открыт для записи",
+    Started: "В процессе обучения",
+    Finished: "Закрыт",
+  };
+
+export const semesterConvertions = {
+    Spring: "Весенний",
+    Autumn: "Осенний"
+}
