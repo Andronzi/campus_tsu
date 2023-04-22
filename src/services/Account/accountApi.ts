cb10ae81635f626d74c86c153eac25b130bb4039
Andronzi <andrey.radioniv.2003@gmail.com>
2023-04-22 13:20:31 +0700

    :sparkles: feat(api): Add getting all users endpoint to accountApi

--- src/services/Account/accountApi.ts
+++ src/services/Account/accountApi.ts
@@ -1,14 +1,19 @@
 import { emptySplitApi } from '../emptySplitApi';
 
 export type Profile = {
-    fullName: string;
-    email: string;
-    birthDate: Date;
+  fullName: string;
+  email: string;
+  birthDate: Date;
 }
 
 export type Login = {
-    email: string;
-    password: string;
+  email: string;
+  password: string;
+}
+
+export type IUser = {
+  id: string;
+  fullName: string;
 }
 
 export type Registration = Profile & Login;
@@ -20,60 +25,69 @@ export type RolesList = {
 }
 
 export const accountApi = emptySplitApi.injectEndpoints({
-    endpoints: (build) => ({
-      getUserProfile: build.query<Profile, string | void>({
-        query: () => '/profile',
-        providesTags: ["Profile"]
-      }),
-      registerUser: build.mutation<{token: string}, Registration>({
-        query(body: Registration) {
+  endpoints: (build) => ({
+    getUserProfile: build.query<Profile, string | void>({
+      query: () => '/profile',
+      providesTags: ["Profile"]
+    }),
+    registerUser: build.mutation<{token: string}, Registration>({
+      query(body: Registration) {
+        return {
+            url: '/registration',
+            method: "POST",
+            body,
+        }
+      },
+    }),
+    loginUser: build.mutation<{ token: string }, Login>({
+      query(body: Login) {
           return {
-              url: '/registration',
+              url: '/login',
               method: "POST",
               body,
           }
-        },
-      }),
-      loginUser: build.mutation<{ token: string }, Login>({
-        query(body: Login) {
-            return {
-                url: '/login',
-                method: "POST",
-                body,
-            }
-        },
-        async onQueryStarted(_, { dispatch, queryFulfilled }) {
-          try {
-            const { data } = await queryFulfilled;
-            localStorage.setItem("token", data.token);
-            // dispatch(emptySplitApi.
-          } catch (err) {
-            console.log(err);
-          }
+      },
+      async onQueryStarted(_, { dispatch, queryFulfilled }) {
+        try {
+          const { data } = await queryFulfilled;
+          localStorage.setItem("token", data.token);
+        } catch (err) {
+          console.log(err);
         }
-      }),
-      logoutUser: build.mutation<void, void>({
-        query() {
-            return {
-                url: '/logout',
-                method: "POST",
-            }
-        },
-        async onQueryStarted(_, { dispatch, queryFulfilled }) {
-          try {
-            await queryFulfilled;
-            dispatch(emptySplitApi.util.resetApiState());
-            localStorage.removeItem("token");
-          } catch (err) {
-            console.log(err);
-          }
-        }
-      }),
-      getUserRoles: build.query<RolesList, void>({
-        query: () => '/roles',
-      })
+      }
     }),
-    overrideExisting: false,
-  })
+    logoutUser: build.mutation<void, void>({
+      query() {
+          return {
+              url: '/logout',
+              method: "POST",
+          }
+      },
+      async onQueryStarted(_, { dispatch, queryFulfilled }) {
+        try {
+          await queryFulfilled;
+          dispatch(emptySplitApi.util.resetApiState());
+          localStorage.removeItem("token");
+        } catch (err) {
+          console.log(err);
+        }
+      }
+    }),
+    getUserRoles: build.query<RolesList, void>({
+      query: () => "/roles",
+    }),
+    getAllUsers: build.query<IUser[], void>({
+      query: () => "/users"
+    })
+  }),
+  overrideExisting: false,
+})
   
-  export const { useGetUserProfileQuery, useRegisterUserMutation, useLoginUserMutation, useLogoutUserMutation, useGetUserRolesQuery } = accountApi
+export const { 
+  useGetUserProfileQuery, 
+  useRegisterUserMutation, 
+  useLoginUserMutation, 
+  useLogoutUserMutation, 
+  useGetUserRolesQuery, 
+  useGetAllUsersQuery 
+} = accountApi
