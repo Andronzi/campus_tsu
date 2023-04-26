c9eba49bb94dd2d43c7329f3acf6dd3f5f259091
Andronzi <andrey.radioniv.2003@gmail.com>
2023-04-26 16:50:54 +0700

    :lipstick: style(ui): Change visibility for students attestations

--- src/pages/courses/items/studentsList.tsx
+++ src/pages/courses/items/studentsList.tsx
@@ -1,9 +1,11 @@
 import Attestation from "@/components/atoms/attestationInfo";
 import Button from "@/components/atoms/button";
+import WithPermission from "@/components/atoms/withPermission";
 import {
   studentStatusColors,
   studentStatusConvertions,
 } from "@/pages/data/student";
+import { useGetUserProfileQuery } from "@/services/Account/accountApi";
 import { Student } from "@/services/Course/types";
 import { FC } from "react";
 
@@ -11,43 +13,65 @@ type StudentsListProps = {
   students: Student[];
 };
 
-const StudentsList: FC<StudentsListProps> = ({ students }) => (
-  <>
-    {students.map((student: Student) => (
-      <li
-        className="flex justify-between px-3 py-2 border-solid border border-slate-300 -mt-px last:rounded-b-md"
-        key={student.email}
-      >
-        <div className="basis-1/3">
-          <p className="font-medium text-md">{student.name}</p>
-          <p className="text-sm text-gray-700">
-            Статус -
-            <span
-              className={`${studentStatusColors[student.status]} font-medium`}
-            >
-              {studentStatusConvertions[student.status]}
-            </span>
-          </p>
-          <p className="text-sm text-gray-700">{student.email}</p>
-        </div>
-        {student.status === "Accepted" && (
-          <div className="flex items-center basis-3/5">
-            <Attestation result={student.midtermResult} />
-            <Attestation result={student.finalResult} />
+const StudentsList: FC<StudentsListProps> = ({ students }) => {
+  const userEmail = useGetUserProfileQuery().data?.email;
+  return (
+    <>
+      {students.map((student: Student) => (
+        <li
+          className="flex justify-between px-3 py-2 border-solid border border-slate-300 -mt-px last:rounded-b-md"
+          key={student.email}
+        >
+          <div className="basis-1/3">
+            <p className="font-medium text-md">{student.name}</p>
+            <p className="text-sm text-gray-700">
+              Статус -
+              <span
+                className={`${studentStatusColors[student.status]} font-medium`}
+              >
+                {studentStatusConvertions[student.status]}
+              </span>
+            </p>
+            <p className="text-sm text-gray-700">{student.email}</p>
           </div>
-        )}
-        {student.status === "InQueue" && (
-          <div>
-            <Button value="Принять" className="w-max" />
-            <Button
-              value="Отклонить заявку"
-              className="w-max bg-red-400 hover:bg-red-600 ml-2"
-            />
-          </div>
-        )}
-      </li>
-    ))}
-  </>
-);
+          {student.status === "Accepted" && (
+            <div className="flex items-center basis-3/5">
+              <WithPermission roles={["Teacher", "Admin"]}>
+                <>
+                  <Attestation
+                    type="Промежуточная"
+                    result={student.midtermResult}
+                  />
+                  <Attestation type="Итоговая" result={student.finalResult} />
+                </>
+              </WithPermission>
+
+              {userEmail === student.email && (
+                <>
+                  <Attestation
+                    type="Промежуточная"
+                    result={student.midtermResult}
+                  />
+                  <Attestation type="Итоговая" result={student.finalResult} />
+                </>
+              )}
+            </div>
+          )}
+          {student.status === "InQueue" && (
+            <WithPermission roles={["Admin", "Teacher"]}>
+              <div>
+                <Button value="Принять" className="w-max" />
+                <Button
+                  value="Отклонить заявку"
+                  className="w-max bg-red-400 hover:bg-red-600 ml-2"
+                />
+              </div>
+            </WithPermission>
+          )}
+        </li>
+      ))}
+    </>
+  );
+};
 
 export default StudentsList;
